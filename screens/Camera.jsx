0d9e0b816cf25a416574a734336b822cd0f7d780
Henri J. Suurorg <hsuurorg@n140-p215.eduroam.kth.se>
2023-04-19 10:53:26 +0200

    aspect ratio on android

--- screens/Camera.jsx
+++ screens/Camera.jsx
@@ -1,19 +1,52 @@
-import { Text, TouchableOpacity, View } from "react-native"
-import { useRef, useState } from "react"
+import { Text, TouchableOpacity, View, Dimensions } from "react-native"
+import { useState, useEffect } from "react"
 import { Camera, CameraType } from "expo-camera"
 import styles from "../styles.js"
+import getAspectRatio from "../utils/getAspectRatio.js"
+import findBestMatchingAspectRatio from "../utils/findBestMatchingAspectRatio.js"
 
-const DisplayCamera = ({ takePic, cameraRef }) => {
+const CameraScreen = ({ takePic, cameraRef }) => {
   const [camera, setCamera] = useState(CameraType.back)
+  const [cameraRatio, setCameraRatio] = useState(null)
+  const [isCameraReady, setIsCameraReady] = useState(false)
+
   const toggleCameraType = () => {
     setCamera((current) =>
       current === CameraType.back ? CameraType.front : CameraType.back
     )
   }
 
+  const onCameraReady = () => {
+    setIsCameraReady(true)
+  }
+
+  useEffect(() => {
+    const setBestMatchingRatio = async () => {
+      const aspectRatio = getAspectRatio()
+
+      if (cameraRef.current && isCameraReady) {
+        const supportedRatios =
+          await cameraRef.current.getSupportedRatiosAsync()
+        const bestRatio = findBestMatchingAspectRatio(
+          supportedRatios,
+          aspectRatio
+        )
+        setCameraRatio(bestRatio)
+      }
+    }
+
+    setBestMatchingRatio()
+  }, [cameraRef, isCameraReady])
+
   return (
     <View style={styles.container}>
-      <Camera type={camera} style={styles.camera} ref={cameraRef}>
+      <Camera
+        type={camera}
+        style={styles.camera}
+        ref={cameraRef}
+        ratio={cameraRatio}
+        onCameraReady={onCameraReady}
+      >
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.capture_button} onPress={takePic}>
             <Text style={styles.text}>Capture</Text>
@@ -30,4 +63,4 @@ const DisplayCamera = ({ takePic, cameraRef }) => {
   )
 }
 
-export default DisplayCamera
+export default CameraScreen
