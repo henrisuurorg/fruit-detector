034afba995ca50e84e84f2a052b4c326205e947c
Fredrik <Fredrik.jogell@live.com>
2023-04-21 13:22:43 +0200

    Added scaling functionality to the moveable square.

--- screens/DisplayPhotoAssets/MovableSquare.js
+++ screens/DisplayPhotoAssets/MovableSquare.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, PanResponder, Dimensions, Button} from 'react-native';
+import { View, StyleSheet, PanResponder, Dimensions} from 'react-native';
+import Animated, { useAnimatedGestureHandler} from 'react-native-reanimated';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 
 
@@ -11,42 +13,53 @@ const startX = (window_width / 2);
 const startY = (window_height / 2);
 
 
+const MovableSquare = ({position, setPosition, squareLength, setSquareLength}) => {
+  const [scale, setScale] = useState(1);
+  const [scaledLength, setScaleLength] = useState(squareLength);
 
 
-const MovableSquare = ({position, setPosition, squareLength}) => {
-  const squareHalfLength = squareLength / 2;
   const panResponder = PanResponder.create({
     onMoveShouldSetPanResponder: () => true,
     onPanResponderMove: (evt, gestureState) => {
+      //console.debug("-------------------------------");
       var x = gestureState.moveX
       var y = gestureState.moveY
-      
+      if (evt.touchHistory.numberActiveTouches == 2) {
+        let point1 = {x: evt.touchHistory.touchBank[0].currentPageX, y: evt.touchHistory.touchBank[0].currentPageY}
+        let point2 = {x: evt.touchHistory.touchBank[1].currentPageX, y: evt.touchHistory.touchBank[1].currentPageY}
+
+        
+        const distance = Math.sqrt( Math.pow( (point2.x - point1.x), 2 ) + Math.pow( (point2.y - point1.y), 2));
+
+        const newScale = Math.max(0.6, Math.min(distance / squareLength, 10));
+        setScale(newScale)
+      }
+      tmp = squareLength * scale
+      if (tmp > window_width) {
+        tmp = window_width;
+      }
+      setScaleLength(tmp);
       // Prevent the box from going outside of the screen
-      if (x - squareHalfLength< 0) x = squareLength / 2;
-      if (y - squareHalfLength < 0) y = squareLength / 2;
-      if (x + squareHalfLength > window_width) x = window_width - squareHalfLength;
-      if (y + squareHalfLength > window_height) y = window_height - squareHalfLength;
+      if (x - scaledLength / 2< 0) x = scaledLength / 2;
+      if (y - scaledLength / 2 < 0) y = scaledLength / 2;
+      if (x + scaledLength / 2 > window_width) x = window_width - scaledLength / 2;
+      if (y + scaledLength / 2 > window_height) y = window_height - scaledLength / 2;
       setPosition({ x, y });
     },
   });
-
-  function measure() {
-    return position;
-  }
-
   return (
-    <View
-      style={[
-        styles.square,
-        {
-            left: position.x  - squareHalfLength,
-            top: position.y  - squareHalfLength,
-            width: squareLength,
-            height: squareLength
-        },
-      ]}
-      {...panResponder.panHandlers}
-    />
+        <View
+          style={[
+            styles.square,
+            {
+                left: position.x  - scaledLength / 2,
+                top: position.y  - scaledLength / 2,
+                width: scaledLength,
+                height: scaledLength,
+            },
+          ]}
+          {...panResponder.panHandlers}
+        />
   );
 };
 
@@ -55,7 +68,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     borderWidth: 8,
     borderRadius: 25,
-    borderColor: "black",
+    borderColor: "blue",
     backgroundColor: 'transparent',
   },
 });
