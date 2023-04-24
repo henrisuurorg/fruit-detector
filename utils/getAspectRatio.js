744a37aefc5aef134e36aab4acf88736d5cf2d53
Henri J. Suurorg <hsuurorg@n174-p19.eduroam.kth.se>
2023-04-24 13:54:04 +0200

    remove duplicats

--- utils/getAspectRatio.js
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from "react-native"
-
-const getAspectRatio = () => {
-  const { width, height } = Dimensions.get("window")
-  return height / width
-}
-
-export default getAspectRatio
