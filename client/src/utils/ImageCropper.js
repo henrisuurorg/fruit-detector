0b1c5123de3619295dd845f4b320b1df8d39e16f
Antonia <antoniawaelken@n163-p107.eduroam.kth.se>
2023-04-26 16:00:59 +0200

    fixed cropping offset

--- client/src/utils/ImageCropper.js
+++ client/src/utils/ImageCropper.js
@@ -12,25 +12,18 @@ const ImageCropper = async ( photo, x, y, length ) => {
     const imgHeight = image.height;                                 // Dimensions of the image
     const imgWidth = image.width;                       
 
-    console.debug("Width: " + width);
-    console.debug("Height: " +  height);
-    console.debug("imgWidth: " + imgWidth);
-    console.debug("imgHeight: " + imgHeight);
-
     const ratioW = parseFloat((imgWidth / width).toFixed(5));       // Ratio of the Width
     const ratioH = parseFloat((imgHeight / height).toFixed(5));     // Ratio of the Height
     //const avgRatio = (ratioH + ratioW) / 2                          // Average ratio to figure out the square length scaler
     let relativeX = (x  - (length / 2)) * ratioW;
-    let relativeY = (y - (length / 2)) * ratioH;
-    let relativeLength = length * ratioH;
-    let relativeWidth = length * ratioW;
-    
-    
+    let relativeY = (y - (length / 2)) * ratioH - (Math.abs(height - imgHeight/ratioW))*2;
+    let relativeLength = length * ratioW;
+      
     // The actual cropping of the image
     const { uri: CropResult } = await manipulateAsync(
         image.uri,
         [
-            {crop: {height: relativeLength, originX: relativeX, originY: relativeY,  width: relativeWidth}},
+            {crop: {height: relativeLength, originX: relativeX, originY: relativeY,  width: relativeLength}},
             {resize: {height: 224, width: 224}}
         ],
         {base64: true}
