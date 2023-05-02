bfe90b71bb359ae5fef404843a9b38782bf4cce9
Henri J. Suurorg <henrijohan.suurorg@gmail.com>
2023-05-02 16:28:37 +0200

    pineapple class_names

--- server/utils/class_names.py
+++ server/utils/class_names.py
@@ -1,5 +1,6 @@
 ripeness_class_names = {
     "banana": ['overripe', 'ripe', 'unripe'],
+    "pineapple": ['overripe', 'ripe', 'unripe'],
 }
 
 detection_class_names = ['apple','apricot','avocado','banana','bell pepper','black berry','black cherry','black currant','blueberry','cherry','clementine','cloudberry','cranberry','dragonfruit','eggplant','elderberry','gooseberry','kiwi','lemon','lime','lingonberry','mango','nectarine','olive','orange','papaya','pea','pear','pineapple','pomegranate','raspberry','strawberry','tomato','vanilla','watermelon','zucchini']
