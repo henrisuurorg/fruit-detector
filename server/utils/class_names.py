a8c1a9a752f8a0f1c979a683ad4025cdb7477d40
Henri J. Suurorg <henrijohan.suurorg@gmail.com>
2023-05-03 15:12:42 +0200

    strawberry model

--- server/utils/class_names.py
+++ server/utils/class_names.py
@@ -2,6 +2,7 @@ ripeness_class_names = {
     "banana": ['overripe', 'ripe', 'unripe'],
     "pineapple": ['overripe', 'ripe', 'unripe'],
     "orange": ['overripe', 'ripe', 'unripe'],
+    "strawberry": ['overripe', 'ripe', 'unripe'],
 }
 
 detection_class_names = ['apple','apricot','avocado','banana','bell pepper','black berry','black cherry','black currant','blueberry','cherry','clementine','cloudberry','cranberry','dragonfruit','eggplant','elderberry','gooseberry','kiwi','lemon','lime','lingonberry','mango','nectarine','olive','orange','papaya','pea','pear','pineapple','pomegranate','raspberry','strawberry','tomato','vanilla','watermelon','zucchini']
