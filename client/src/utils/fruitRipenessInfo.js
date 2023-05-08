f6a695d4025e20733a3096ab3026dd4a44eee27e
Kevin Wang <kevin@n138-p222.eduroam.kth.se>
2023-05-08 14:24:17 +0200

    Added new feature

--- client/src/utils/fruitRipenessInfo.js
+++ client/src/utils/fruitRipenessInfo.js
@@ -1,112 +1,112 @@
 const fruitRipenessInfo = {
   apple: [
-    "check its color - should have no green backcolour",
-    "give it a gentle squeeze - should have a slight softness",
-    "if it's a green apple it should have a brght glowy green colour",
+    "Check its color - should have no green backcolour",
+    "Give it a gentle squeeze - should have a slight softness",
+    "If it's a green apple it should have a brght glowy green colour",
   ],
   apricot: [
-    "check its color - should be golden or orange",
-    "give it a gentle squeeze - should have a little give",
-    "smell it - should have a strong, sweet smell",
+    "Check its color - should be golden or orange",
+    "Give it a gentle squeeze - should have a little give",
+    "Smell it - should have a strong, sweet smell",
   ],
   avocado: [
-    "check its color - should be nearly black",
-    "give it a gentle squeeze - should have a little give",
-    "check its texture - should have bumpy texture",
+    "Check its color - should be nearly black",
+    "Give it a gentle squeeze - should have a little give",
+    "Check its texture - should have bumpy texture",
   ],
   banana: [
-    "check its color - ","Naturally ripened: should be dark yellow with some dark spots","Artificially ripened: should be lemon yellow with some dark spots",
-    "check its stalk - ","Naturally ripened: should have a black or brown stalk","Artificially ripened: should have green stalk",
+    "Check its color - ","Naturally ripened: should be dark yellow with some dark spots","Artificially ripened: should be lemon yellow with some dark spots",
+    "Check its stalk - ","Naturally ripened: should have a black or brown stalk","Artificially ripened: should have green stalk",
   ],
   "bell pepper": [
-    "check its color: should be fully yellow, orange, or red",
-    "give it a gentle squeeze: ripe peppers are firm, but slightly soft to the touch.",
-    "hold the pepper: A ripe pepper will feel heavy for its size",
-    "check its stem: If the stem is green and firm, the pepper may not be fully ripe. If the stem is brown or starting to wilt, the pepper may be overripe.",
+    "Check its color - should be fully yellow, orange, or red",
+    "Give it a gentle squeeze - ripe peppers are firm, but slightly soft to the touch.",
+    "Hold the pepper - A ripe pepper will feel heavy for its size",
+    "Check its stem - If the stem is green and firm, the pepper may not be fully ripe. If the stem is brown or starting to wilt, the pepper may be overripe.",
   ],
   "black berry": [
-    "check its colour: Blackberries should be a deep, rich black color. ",
-    "shine: Look for blackberries that are shiny and have a glossy appearance.",
-    "give it a gentle squeeze: Ripe blackberries should be firm, but not too hard or too soft.",
-    "Smell it: Ripe blackberries should have a sweet, fruity aroma. ",
-    "Taste: Ripe blackberries should be sweet and juicy."
+    "Check its colour - Blackberries should be a deep, rich black color. ",
+    "Shine - Look for blackberries that are shiny and have a glossy appearance.",
+    "Give it a gentle squeeze - Ripe blackberries should be firm, but not too hard or too soft.",
+    "Smell it - Ripe blackberries should have a sweet, fruity aroma. ",
+    "Taste - Ripe blackberries should be sweet and juicy."
   ],
   "black cherry": [
-    "Color: should have a deep, dark color that is almost black.", 
-    "Shine: should be shiny and have a glossy appearance",
-    "give it a gentle squeeze: Ripe cherries should be firm to the touch but not too hard. ",
-    "Stem: If the stem is green and firmly attached, the cherry may not be fully ripe. If the stem is brown or starting to wilt, the cherry may be overripe.",
-    "Taste: Ripe cherries should be sweet and juicy."
+    "Color - should have a deep, dark color that is almost black.", 
+    "Shine - should be shiny and have a glossy appearance",
+    "Give it a gentle squeeze - Ripe cherries should be firm to the touch but not too hard. ",
+    "Stem - If the stem is green and firmly attached, the cherry may not be fully ripe. If the stem is brown or starting to wilt, the cherry may be overripe.",
+    "Taste - Ripe cherries should be sweet and juicy."
   ],
   "black currant": [
-    "Color: should have a deep, dark color that is almost black.", 
-    "give it a gentle squeeze: should be firm to the touch but not too hard. ",
-    "Taste: Ripe cherries should be slightly tart and juicy, with a sweet flavor."
+    "Color - should have a deep, dark color that is almost black.", 
+    "Give it a gentle squeeze - should be firm to the touch but not too hard. ",
+    "Taste - Ripe cherries should be slightly tart and juicy, with a sweet flavor."
   ],
   blueberry: [
-    "Color: should have a deep blue color that is almost black", 
-    "Bloom: should have a light blue-grayish bloom on their surface.",
-    "Size: Ripe blueberries should be plump and round. ",
-      "give it a gentle squeeze: Ripe cherries should be firm to the touch but not too hard. ",
-    "Taste: should be sweet and juicy with a slightly tart taste."
+    "Color - should have a deep blue color that is almost black", 
+    "Bloom - should have a light blue-grayish bloom on their surface.",
+    "Size - Ripe blueberries should be plump and round. ",
+    "Give it a gentle squeeze - Ripe cherries should be firm to the touch but not too hard. ",
+    "Taste - should be sweet and juicy with a slightly tart taste."
 
   ],
   cherry: [
-    "Color: should have a deep, vibrant color that is bright and glossy", 
-    "Stem: If the stem is green and firmly attached, the cherry may not be fully ripe. If the stem is brown or starting to wilt, the cherry may be overripe.",
-    "Size: should be plump and round. ",
-    "give it a gentle squeeze: Ripe cherries should be firm to the touch but not too hard. ",
-    "Taste: should be sweet and juicy."
+    "Color - should have a deep, vibrant color that is bright and glossy", 
+    "Stem - If the stem is green and firmly attached, the cherry may not be fully ripe. If the stem is brown or starting to wilt, the cherry may be overripe.",
+    "Size - should be plump and round. ",
+    "Give it a gentle squeeze - Ripe cherries should be firm to the touch but not too hard. ",
+    "Taste - should be sweet and juicy."
   ],
   clementine: [
-    "check its color - should be bright orange",
-    "give it a gentle squeeze - should have a little give",
+    "Check its color - should be bright orange",
+    "Give it a gentle squeeze - should have a little give",
     "smell it - should have a sweet, citrusy aroma",
   ],
   cloudberry: [
-    "Color: Ripe cloudberry fruit is usually a golden-orange color, with a soft, slightly furry texture.", 
-    "Smell it: sweet and slightly tangy aroma that is pleasant and not too strong. ",
-    "give it a gentle squeeze: should be soft and juicy ",
-    "Taste: should be sweet with a hint of tartness"
+    "Color - Ripe cloudberry fruit is usually a golden-orange color, with a soft, slightly furry texture.", 
+    "Smell it - sweet and slightly tangy aroma that is pleasant and not too strong. ",
+    "give it a gentle squeeze - should be soft and juicy ",
+    "Taste - should be sweet with a hint of tartness"
   ],
   cranberry: [
-    "Color:  should be a deep, rich red color, with a slightly glossy appearance", 
-    "texture: should have a smooth, plump appearance",
-    "Smell it: sweet and slightly tangy aroma that is pleasant and not too strong. ",
-    "give it a gentle squeeze: Ripe cranberries should be firm to the touch, with a slight bounce when dropped. ",
-    "Taste: should be slightly tart, but not overly sour"
+    "Color - should be a deep, rich red color, with a slightly glossy appearance", 
+    "texture - should have a smooth, plump appearance",
+    "Smell it - sweet and slightly tangy aroma that is pleasant and not too strong. ",
+    "Give it a gentle squeeze - Ripe cranberries should be firm to the touch, with a slight bounce when dropped. ",
+    "Taste - should be slightly tart, but not overly sour"
   ],
   dragonfruit: [
-    "Color:  should be a bright, vibrant color", 
-    "Smell it: Ripe dragonfruit has a sweet and slightly floral aroma. ",
-    "give it a gentle squeeze: Dragonfruit should be firm to the touch, but not too hard",
-    "Scales: If the scales are starting to wither or turn brown, the fruit may be overripe."
+    "Color - should be a bright, vibrant color", 
+    "Smell it - Ripe dragonfruit has a sweet and slightly floral aroma. ",
+    "give it a gentle squeeze - Dragonfruit should be firm to the touch, but not too hard",
+    "Scales - If the scales are starting to wither or turn brown, the fruit may be overripe."
   ],
   eggplant: [
-    "Color:  should have smooth, shiny, and have a uniform color", 
-    "Stem: should be firm and not dried out.",
-    "give it a gentle squeeze: Eggplants should be firm to the touch, with a slight give when pressed",
-    "weight: ripe eggplant should be heavy for their size"
+    "Color - should have smooth, shiny, and have a uniform color", 
+    "Stem - should be firm and not dried out.",
+    "give it a gentle squeeze - Eggplants should be firm to the touch, with a slight give when pressed",
+    "weight - ripe eggplant should be heavy for their size"
   ],
   elderberry: [
-    "Color:  should be a deep, rich purple-black color", 
-    "texure: should have a smooth, plump appearance. ",
-    "give it a gentle squeeze: Elderberries should be firm to the touch, but not too hard. ",
-    "Taste: Ripe elderberries should be slightly tart, with a sweet flavor. "
+    "Color - should be a deep, rich purple-black color", 
+    "Texure - should have a smooth, plump appearance. ",
+    "Give it a gentle squeeze - Elderberries should be firm to the touch, but not too hard. ",
+    "Taste - Ripe elderberries should be slightly tart, with a sweet flavor. "
   ],
   gooseberry: [
-    "Color:  should be a greenish-yellow or pinkish-red color ", 
-    "texure: should have a smooth, plump appearance. ",
-    "give it a gentle squeeze: should be firm to the touch, with a slight give when pressed ",
-    "Taste: should be slightly tart, with a sweet flavor"
+    "Color -  should be a greenish-yellow or pinkish-red color ", 
+    "texure - should have a smooth, plump appearance. ",
+    "give it a gentle squeeze - should be firm to the touch, with a slight give when pressed ",
+    "Taste - should be slightly tart, with a sweet flavor"
 
   ],
   kiwi: [
-    "Color: should be brown and fuzzy", 
-    "texure: should have smooth, unwrinkled skin",
-    "give it a gentle squeeze: should be slightly soft to the touch when ripe, but not mushy.",
-"Weight: Ripe kiwifruit should feel heavy for their size.",
-    "smell it: Ripe kiwifruit will have a sweet aroma."
+    "Color - should be brown and fuzzy", 
+    "texure - should have smooth, unwrinkled skin",
+    "give it a gentle squeeze - should be slightly soft to the touch when ripe, but not mushy.",
+    "Weight - Ripe kiwifruit should feel heavy for their size.",
+    "smell it - Ripe kiwifruit will have a sweet aroma."
   ],
   lemon: [
     "check its color - should be bright yellow in color, with no green or brown areas on the skin.",
@@ -134,10 +134,10 @@ const fruitRipenessInfo = {
     "smell it - should have a sweet, citrusy aroma",
   ],//Done
   olive: [
-    "Table olives: Table olives are typically harvested when they are fully ripe, but not overripe. They should be plump and firm to the touch, with a shiny and smooth skin. The color of the olives will depend on the variety, but in general, ripe table olives will be a deep purple or black color.",
-    "Olive oil: For olive oil production, the olives are typically harvested when they are fully ripe or slightly overripe. The color of the olives will depend on the variety, but in general, ripe olives for oil production will be a mix of green and purple or black.",
-    "Pickling: Olives for pickling can be harvested at different stages of ripeness, depending on the desired flavor and texture. For example, green olives are typically harvested when they are still unripe and firm, while black olives are harvested when they are fully ripe and softer.",
-  ],//Done but a bit tricky
+    "Taste (Table olives) - should have a rich and complex flavor, with a balance of bitterness, saltiness, and fruitiness.",
+     "Texture - They should be plump and firm to the touch, with a shiny and smooth skin", 
+     "Color - The color will depend on the variety, but in general, ripe table olives will be a deep purple or black color.",
+  ],//Done
   orange: [
     "check its color - should be bright orange",
     "give it a gentle squeeze - should have a little give",
