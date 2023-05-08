6c37edfc15993a6697a65fc8287a4ea4dbf629af
Fredrik <Fredrik.jogell@live.com>
2023-05-08 14:26:31 +0200

    Added Alternative Percentages with toggle

--- client/src/components/AltFruitList/AltFruitList.jsx
+++ client/src/components/AltFruitList/AltFruitList.jsx
@@ -1,18 +1,23 @@
-import { View } from "react-native"
+import { TouchableOpacity, View } from "react-native"
+import { useState } from 'react'
 import BaseText from "../BaseText/BaseText"
 import fruitEmojis from "../../utils/fruitEmojis"
 import addArticle from "../../utils/addArticle"
 
-const renderItem = (item) => (
-  <BaseText key={item.class}>{`\t${fruitEmojis[item.class]} ${
-    item.class
-  }`}</BaseText>
+const renderItem = (item, showConf) => (
+  <BaseText key={item.class}>{`\t${fruitEmojis[item.class]} ${item.class}`} {showConf && " (" + item.confidence + ")"}</BaseText>
 )
 
 const AltFruitList = ({ fruit, alts, confi }) => {
+  const [showConf, setShowConf] = useState(false)
   const altsSorted = alts.sort(
     (a, b) => parseFloat(b.confidence) - parseFloat(a.confidence)
   )
+
+  const toggleConfidence = async () => {
+    setShowConf(!showConf);
+  }
+
   return (
     <View>
       <BaseText>
@@ -22,7 +27,9 @@ const AltFruitList = ({ fruit, alts, confi }) => {
       {alts.length > 0 && (
         <View>
           <BaseText style={{ marginTop: 20 }}>It also could be...</BaseText>
-          {altsSorted.map(renderItem)}
+          <TouchableOpacity onPress={() => toggleConfidence()}>
+          {altsSorted.map(item => renderItem(item, showConf))}
+          </TouchableOpacity>
           <BaseText style={{ marginVertical: 5 }}>
             or something completely different.
           </BaseText>
