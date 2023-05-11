92f9cae94a3974ce02de221d2ee04285e054c0dd
Seolli Kim <seollikim01@gmail.com>
2023-05-11 14:41:38 +0200

    Avocado ripeness model implemented

--- server/utils/class_names.py
+++ server/utils/class_names.py
@@ -3,6 +3,8 @@ ripeness_class_names = {
     "pineapple": ['overripe', 'ripe', 'unripe'],
     "orange": ['overripe', 'ripe', 'unripe'],
     "strawberry": ['overripe', 'ripe', 'unripe'],
+    "Avocado": ['ripe', 'unripe'],
+    
 }
 
 detection_class_names = ['apple', 'avocado', 'banana', 'bell pepper', 'black berry', 'blueberry', 'cherry', 'clementine', 'coconut', 'cranberry', 'date', 'dragonfruit', 'durian', 'eggplant', 'fig', 'grape', 'grapefruit', 'guava', 'jalapeno', 'kiwi', 'lemon', 'lime', 'lychee', 'mango', 'nectarine', 'olive', 'orange', 'papaya', 'passion fruit', 'pear', 'physalis', 'pineapple', 'pumpkin', 'raspberry', 'strawberry', 'tomato', 'watermelon', 'zucchini']
